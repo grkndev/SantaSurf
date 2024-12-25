c77a7614938b63427ed789483d4561909a4200f3
GweepCreative <gweepcreative@gmail.com>
2024-12-25 12:53:43 +0300

    setup #2

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import React, { Suspense } from "react";
-import { Model } from "@/components/Model/SantaClauss/Model";
+import { Model } from "@/components/Models/SantaClauss/Model";
 import * as THREE from "three";
 import Animated, {
   useSharedValue,
@@ -12,24 +12,6 @@ import Animated, {
 import { Pressable, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-// import { useAnimatedSensor, SensorType } from "react-native-reanimated";
-
-// function Test(props) {
-//   const mesh = React.useRef();
-//   useFrame((state, delta) => {
-//     let { x, y, z } = props.animatedSensor.sensor.value;
-//     x = ~~(x * 100) / 5000;
-//     y = ~~(y * 100) / 5000;
-//     mesh.current.rotation.x += x;
-//     mesh.current.rotation.y += -y;
-//   });
-
-//   return (
-//     <mesh {...props} ref={mesh}>
-//       <Model rotation={[0.8, 1.565, 0]} scale={0.002} />
-//     </mesh>
-//   );
-// }
 function AnimatedModel(props) {
   const mesh = React.useRef();
   const currentOffset = useSharedValue(0);
@@ -47,13 +29,7 @@ function AnimatedModel(props) {
 }
 
 export default function Index() {
-  //   const animatedSensor = useAnimatedSensor(SensorType.GYROSCOPE, {
-  //     interval: 100,
-  //   });
   const offset = useSharedValue(0);
-  // const animatedStyles = useAnimatedStyle(() => ({
-  //   transform: [{ translateX: (offset.value < 0 ? -offset.value : offset.value) * 100 }],
-  // }));
   const changeOffset = (value) => {
     offset.value = withSpring(value);
   };
@@ -89,26 +65,12 @@ export default function Index() {
         </View>
       </View>
       <Animated.View className={"flex-1 flex "}>
-        {/* <Animated.View
-          style={[
-            {
-              top:0,
-              left:0,
-              backgroundColor: "black",
-              height: 100,
-              width: 100,
-              position: "absolute",
-            },
-            animatedStyles,
-          ]}
-        /> */}
         <Canvas>
           <ambientLight />
           <pointLight position={[10, 10, 10]} decay={0} intensity={Math.PI} />
 
           <Suspense fallback={null}>
             <AnimatedModel offset={offset} />
-            {/* <Model rotation={[0.8, 1.565, 0]} scale={0.002} /> */}
           </Suspense>
         </Canvas>
       </Animated.View>
